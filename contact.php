3bb5bd914663986eaa973245b3ea7adb0d025258
unknown <ruicosta607@gmail.com>
2022-01-16 01:46:18 +0000

    Security & Front Work

--- contact.php
+++ contact.php
@@ -102,7 +102,6 @@
                             <form action="#">
                                 <input type="text" placeholder="Name">
                                 <input type="text" placeholder="Email">
-                                <input type="text" placeholder="Website">
                                 <textarea placeholder="Message"></textarea>
                                 <button type="submit" class="site-btn">Send Message</button>
                             </form>
@@ -121,63 +120,6 @@
     </div>
 </section>
 <!-- Contact Section End -->
-
-<!-- Instagram Begin -->
-<div class="instagram">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                <div class="instagram__item set-bg" data-setbg="img/instagram/insta-1.jpg">
-                    <div class="instagram__text">
-                        <i class="fa fa-instagram"></i>
-                        <a href="#">@ ashion_shop</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                <div class="instagram__item set-bg" data-setbg="img/instagram/insta-2.jpg">
-                    <div class="instagram__text">
-                        <i class="fa fa-instagram"></i>
-                        <a href="#">@ ashion_shop</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                <div class="instagram__item set-bg" data-setbg="img/instagram/insta-3.jpg">
-                    <div class="instagram__text">
-                        <i class="fa fa-instagram"></i>
-                        <a href="#">@ ashion_shop</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                <div class="instagram__item set-bg" data-setbg="img/instagram/insta-4.jpg">
-                    <div class="instagram__text">
-                        <i class="fa fa-instagram"></i>
-                        <a href="#">@ ashion_shop</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                <div class="instagram__item set-bg" data-setbg="img/instagram/insta-5.jpg">
-                    <div class="instagram__text">
-                        <i class="fa fa-instagram"></i>
-                        <a href="#">@ ashion_shop</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-lg-2 col-md-4 col-sm-4 p-0">
-                <div class="instagram__item set-bg" data-setbg="img/instagram/insta-6.jpg">
-                    <div class="instagram__text">
-                        <i class="fa fa-instagram"></i>
-                        <a href="#">@ ashion_shop</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Instagram End -->
 
 <!-- Footer Section Begin -->
 <footer class="footer">
--- main.php
+++ main.php
@@ -1,27 +1,22 @@
+  
+  <?php if(isset($_GET["message"]) && $_GET["message"]=="bloqueado"){
+    echo "<br><center style='font-size: 20px'>De momento, a sua conta encontra se bloqueada !</center>" ;
+  }?>
   <!-- Categories Section Begin -->
-  <?php require("category.php");?>
+  <!--  <?php require("category.php");?>-->
 <!-- Categories Section End -->
 
 <!-- Product Section Begin -->
 <?php require("prodSection.php");?>
 <!-- Product Section End -->
 
-<!-- Banner Section Begin -->
+<!--
 <?php require("banner.php"); ?>
-<!-- Banner Section End -->
 
-<!-- Trend Section Begin -->
 <?php require("trend.php");?>
-<!-- Trend Section End -->
 
-<!-- Discount Section Begin -->
 <?php require("discount.php"); ?>
-<!-- Discount Section End -->
-
-<!-- Services Section Begin -->
 <?php require("services.php"); ?>
-<!-- Services Section End -->
 
-<!-- Instagram Begin -->
 <?php require("instagram.php"); ?>
-<!-- Instagram End -->
+Instagram End -->
